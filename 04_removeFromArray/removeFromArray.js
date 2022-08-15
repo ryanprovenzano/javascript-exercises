89fa04c67c47a0bcb7c80e910a3b79d92f597201
Ryan Provenzano <ryan.provenz@gmail.com>
2022-08-15 18:13:55 -0400

    exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) { //Removes the optional arguments from arr
+    if (arguments.length === 0) return arr;
+    
+    const args = Array.from(arguments);
+    args.shift();
+    return arr.filter( element => !args.includes(element) );
 
+    
 };
 
 // Do not edit below this line
