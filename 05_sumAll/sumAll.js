ccd6976f7744a1007ea249abc38332f615355988
Ryan Provenzano <ryan.provenz@gmail.com>
2022-08-17 14:13:51 -0400

    exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //Validating inputs
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0) return 'ERROR'; //No negative inputs allowed
+
+    // summing numbers
+    let largest = 0;
+    let smallest = 0;
+    if (num1 > num2) largest = num1, smallest = num2;
+    else largest = num2, smallest = num1;
+
+    let sum = 0;
+    for (let i = smallest; i <= largest; i++){
+        sum += i;
+    }
+    return sum;
+
 
 };
 
